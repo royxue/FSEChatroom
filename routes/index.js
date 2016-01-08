92882ac6fc91558b52422e53b8cabb0c95b2f9ee
Lijun Xue <xljroy@gmail.com>
2016-01-07 18:20:09 -0800

    Index and layout update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.session.username){
-    res.render('posts');
+    res.redirect('/posts');
   }
   res.render('index', { title: 'FSEChatroom' });
 });
